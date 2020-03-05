f8051eb3504a1125db06c9bf0f14dc441ebf5f5e
Sander Bollen <sander@sndrtj.eu>
2020-03-06 00:26:05 +0100

    Fix permission error and allow interactive mode in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+set -e
 
-/opt/fahclient/FAHClient \
-    --user=${USER} \
-    --team=${TEAM} \
-    --passkey=${PASSKEY} \
-    --gpu=${ENABLE_GPU} \
-    --smp=${ENABLE_SMP} \
-    --power=full \
-    --gui-enabled=false \
-    ${@}
+# if a command was specified, we run that command. E.g. helpful when we do
+# docker run -it <container> /bin/bash
+if [ "${1}" ]
+then
+  exec "$@"
+# otherwise we attempt to run boinc.
+else
+  /opt/fahclient/FAHClient \
+      --user="${USER}" \
+      --team="${TEAM}" \
+      --passkey="${PASSKEY}" \
+      --gpu="${ENABLE_GPU}" \
+      --smp="${ENABLE_SMP}" \
+      --power=full \
+      --gui-enabled=false \
+      "${@}"
+fi
