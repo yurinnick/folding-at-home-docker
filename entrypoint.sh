c373fd500f0f4348695ece78cffd3eb4bc2bfa8e
Sander Bollen <sander@sndrtj.eu>
2020-03-06 23:42:44 +0100

    undo if/else

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 set -e
 
-# if a command was specified, we run that command. E.g. helpful when we do
-# docker run -it <container> /bin/bash
-if [ "${1}" ]
-then
-  exec "$@"
-# otherwise we attempt to run fahclient.
-else
-  /opt/fahclient/FAHClient \
-      --user="${USER}" \
-      --team="${TEAM}" \
-      --passkey="${PASSKEY}" \
-      --gpu="${ENABLE_GPU}" \
-      --smp="${ENABLE_SMP}" \
-      --power=full \
-      --gui-enabled=false \
-      "${@}"
-fi
+/opt/fahclient/FAHClient \
+    --user="${USER}" \
+    --team="${TEAM}" \
+    --passkey="${PASSKEY}" \
+    --gpu="${ENABLE_GPU}" \
+    --smp="${ENABLE_SMP}" \
+    --power=full \
+    --gui-enabled=false \
+    "${@}"
